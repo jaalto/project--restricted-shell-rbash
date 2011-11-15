34f9d0fb4bd737dd1b47633c6e388da28e93c188
Jari Aalto <jari.aalto@cante.net>
2011-11-15 17:30:34 +0200

    makefile.sh: check root, umask, other fixes

--- makefile.sh
+++ makefile.sh
@@ -12,11 +12,20 @@ Main ()
     LOGIN=$1
 
     if [ ! "$LOGIN" ]; then
-	echo "ERROR: Which login name to create for restricted shell?"
+	echo "ERROR: Which login name to create for restricted shell?" >&2
 	return 1
     fi
 
+    if [ ! "$test" ]; then
+	if [ "$(id -u -n)" != "root" ]; then
+	    echo "ERROR: This file must be run by root" >&2
+	    return 1
+	fi
+    fi
+
+    # Don't use -m option because it would copy skeleton files.
     Run useradd -d /home/dummy -s /bin/rbash dummy
+    mkdir -p /home/dummy
 
     Run mkdir -p /usr/local/bin/restricted
 
@@ -28,6 +37,8 @@ Main ()
 
 	[ "$path" ] && Run ln -s $path $cmd
     done
+
+    umask 022
 
     Run cd ~$LOGIN || return 1
 
