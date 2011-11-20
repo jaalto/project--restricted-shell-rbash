2583b8a7593b5d5f5a8a112a60792d5f5999a373
Jari Aalto <jari.aalto@cante.net>
2011-11-21 01:32:16 +0200

    makefile.sh: rewrite. Support multiple invocations

--- makefile.sh
+++ makefile.sh
@@ -2,30 +2,30 @@
 #
 # To create restricted shell
 
+pwd=$( cd $(dirname $0) ; pwd )
+
 Run ()
 {
     ${test+echo} eval "$@"
 }
 
-Main ()
+IsUser ()
 {
-    LOGIN=$1
+    getent passwd "$1" > /dev/null 2>&1
+}
 
-    if [ ! "$LOGIN" ]; then
-	echo "ERROR: Which login name to create for restricted shell?" >&2
-	return 1
+MakeUser ()
+{
+    if ! IsUser "$1" ; then
+        # Don't use -m option because it would copy skeleton files.
+	Run useradd -d /home/dummy -s /bin/rbash "$1"
+	mkdir -p "/home/$1"
     fi
+}
 
-    if [ ! "$test" ]; then
-	if [ "$(id -u -n)" != "root" ]; then
-	    echo "ERROR: This file must be run by root" >&2
-	    return 1
-	fi
-    fi
-
-    # Don't use -m option because it would copy skeleton files.
-    Run useradd -d /home/dummy -s /bin/rbash dummy
-    mkdir -p /home/dummy
+MakeBin ()
+{
+    [ -d /usr/local/bin/restricted ] && return 0
 
     Run mkdir -p /usr/local/bin/restricted
 
@@ -37,25 +37,52 @@ Main ()
 
 	[ "$path" ] && Run ln -s $path $cmd
     done
+}
+
+Copy ()
+{
+    umask 022
+
+    for elt in ./.[a-z]*
+    do
+	if [ -d "$elt" ]; then
+	    Run cp --verbose -r "$elt" ~"$1"/
+	else
+	    Run cp --verbose "$elt" ~"$1"/
+	fi
+    done
+}
+
+Main ()
+{
+    LOGIN=$1
+
+    if [ ! "$LOGIN" ]; then
+	echo "ERROR: Which login name to use for restricted shell?" >&2
+	return 1
+    fi
+
+    if [ ! "$test" ]; then
+	if [ "$(id -u -n)" != "root" ]; then
+	    echo "ERROR: This file must be run by root" >&2
+	    return 1
+	fi
+    fi
+
+    MakeUser "$LOGIN"
+    MakeBin
+    Copy "$LOGIN"
 
     Run cd ~$LOGIN || return 1
 
-    umask 022
-
-    Run mkdir .ssh
-    Run "echo PATH=/usr/local/bin/restricted > .ssh/environment"
-
-    Run touch .bashrc .bash_logout
-    Run "echo PATH=/usr/local/bin/restricted > .bash_profile"
-
     Run chown root:root .bash* .ssh .ssh/environment
     Run chown root:root .
-    Run chmod 755 . .bash* .ssh/*
+    Run chmod 755 . .bash* .ssh .ssh/*
 
     umask 077
 
-    Run touch .rhosts .shosts
     Run chown root:root .rhosts .shosts
+    chmod 600 .rhosts .shosts
 }
 
 Main "$@"
